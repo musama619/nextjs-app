841a9d6f7a32f19b31e08d62a930cc61b2f62b9a
musama619 <musama6191@gmail.com>
2022-04-27 22:54:02 +0500

    dynamic params, getStaticPaths

---
+++ pages/posts/index.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+function PostList({ posts }) {
+  return (
+    <>
+      <h1>List of Posts</h1>
+      {posts.map((post, index) => {
+        return (
+          <div key={post.id}>
+            <Link href={`posts/${post.id}`} passHref>
+              <h3>{post.title}</h3>
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default PostList;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data.slice(0, 3),
+    },
+  };
+}
